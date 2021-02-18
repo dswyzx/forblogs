9eb0665ff1bfc1c1fedf33c5cc0ffdb37fa9d0e7
dswyzx <dswyzx@users.noreply.github.com>
2021-02-18 14:23:42 +0800

    init and code

---
+++ ConsoleApp1/p2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://localhost:44343/home/GetUserInfo";
+
+            string tes = "k=1%2b1";
+
+            //string oth = HttpUtility.UrlEncode(tes);
+            //string oth1 = HttpUtility.UrlDecode(oth);
+            //string oth2 = HttpUtility.UrlDecode(tes);
+
+            HttpClient client = new HttpClient();
+            var hc = new StringContent(tes, Encoding.UTF8, "application/x-www-form-urlencoded");
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Content = hc;
+
+            HttpResponseMessage response = await client.PostAsync(url, hc);
+            string ret = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(ret);
+
+        }
+    }
+}
---
+++ WebApplication1c/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication1c.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult GetUserInfo(FormCollection collection)
+        {
+            string username = collection["k"];
+            return Content(username);
+        }
+    }
+}
